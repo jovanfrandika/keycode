5c26b7f280ae578741f235855942ce44260a1012
uryella <geofanvandicca@gmail.com>
2020-10-05 07:31:55 +0700

    change api address

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  // baseURL: "https://localhost:4000"
-  baseURL: "http://128.199.142.144/"
+  baseURL: "https://128.199.142.144/"
 });
 
 export default axios;
